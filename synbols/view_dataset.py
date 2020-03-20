607a85076940e808b8a1949a58c047832ce60714
recursix <alex.lacoste.shmu@gmail.com>
2020-03-20 13:55:49 -0400

    Renaming files

--- synbols/view_dataset.py
+++ synbols/view_dataset.py
@@ -1,38 +1,56 @@
-from data_io import pack_dataset
-from generate_dataset import generate_char_grid
-from viz_dataset import plot_dataset
-import synbols
 import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+from data_io import load_dataset_jpeg_sequential, pack_dataset
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def _extract_axis(y, h_axis, v_axis):
+    h_values = np.unique([attr[h_axis] for attr in y])
+    v_values = np.unique([attr[v_axis] for attr in y])
+
+    map = defaultdict(list)
+
+    for i, attr in enumerate(y):
+        map[(attr[h_axis], attr[v_axis])].append(i)
+
+    return map, h_values, v_values
+
+
+def plot_dataset(x, y, h_axis='char', v_axis='font', name="dataset", n_row=20, n_col=30, rng=np.random):
+    fig = plt.figure(name)
+    # plt.axis('off')
+
+    attr_map, h_values, v_values = _extract_axis(y, h_axis, v_axis)
+    h_values = rng.choice(h_values, np.minimum(n_col, len(h_values)), replace=False)
+    v_values = rng.choice(v_values, np.minimum(n_row, len(v_values)), replace=False)
+
+    img_grid = []
+    blank_image = np.zeros(x.shape[1:], dtype=x.dtype)
+
+    for v_value in v_values:
+        img_row = []
+        for h_value in h_values:
+            if (h_value, v_value) in attr_map.keys():
+
+                idx = attr_map[(h_value, v_value)][0]
+                img_row.append(x[idx])
+            else:
+                img_row.append(blank_image)
+
+        img_grid.append(np.hstack(img_row))
+
+    img_grid = np.vstack(img_grid)
+    plt.imshow(img_grid)
+    plt.xlabel(h_axis)
+    plt.ylabel(v_axis)
+
+    fig.tight_layout()
+    plt.show()
+
 
 if __name__ == "__main__":
-    alphabet = 'latin'  # TODO missing upper cases
-    # alphabet = 'telugu'  # TODO the circle in some chars is not rendered. (bottom ones)
-    # alphabet = 'thai'
-    # alphabet = 'vietnamese'
-    # alphabet = 'arabic'  # TODO: missing chars in fonts
-    # alphabet = 'hebrew'
-    # alphabet = 'khmer' # TODO: see comment in googlefonts
-    # alphabet = 'tamil'
-    # alphabet = 'gujarati' # TODO: one font to remove and one blank in another font
-    # alphabet = 'bengali'
-    # alphabet = 'malayalam'
-    # alphabet = 'korean' # TODO: huge amount of missing chars
-    # alphabet = 'chinese-simplified'
-    # alphabet = 'greek'
-    # alphabet = 'cyrillic'
-
-
-    fg = synbols.Camouflage(stroke_angle=0.5)
-    bg = synbols.Camouflage(stroke_angle=1.)
-
-    ## Uncomment to remove background
-    # fg, bg = None, None
-
-    ## Uncomment for a smoother background
-    fg, bg = 'gradient', 'gradient'
-
-    kwargs = dict(foreground=fg, background=bg, is_bold=True, scale=(1.3, 1.3),
-                  resolution=(32, 32), rng=np.random.RandomState(42))
-
-    x, y = pack_dataset(generate_char_grid('latin', n_font=15, n_char=10, **kwargs))
-    plot_dataset(x, y, name=alphabet, h_axis='char', v_axis='font', rng=np.random.RandomState(42))
+    x, y = pack_dataset(load_dataset_jpeg_sequential('../camouflage_n=10000.zip'))
+    plot_dataset(x, y)
