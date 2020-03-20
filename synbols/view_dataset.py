1a7c4ce0bc2fff97bb516cf9bafa21217a90a240
recursix <alex.lacoste.shmu@gmail.com>
2020-03-20 13:01:14 -0400

    Simplify using new visualization tools

--- synbols/view_dataset.py
+++ synbols/view_dataset.py
@@ -1,44 +1,35 @@
-import matplotlib.pyplot as plt
-import numpy as np
+from data_io import pack_dataset
+from generate_dataset import generate_char_grid
+from viz_dataset import plot_dataset
 import synbols
-
-
-def plot_dataset(ds, name="dataset", class_stride=1, save_path="."):
-    fig = plt.figure(name)
-    plt.axis('off')
-
-    img_grid = []
-    for one_class in ds[::class_stride]:
-        img_grid.append(np.hstack([x for x in one_class]))
-    img_grid = np.vstack(img_grid)
-    plt.imshow(img_grid)
-
-    fig.tight_layout()
-    plt.savefig("%s/%s.png" % (save_path, name), dpi=1000)
-
+import numpy as np
 
 if __name__ == "__main__":
+    alphabet = 'latin'  # TODO missing upper cases
+    # alphabet = 'telugu'  # TODO the circle in some chars is not rendered. (bottom ones)
+    # alphabet = 'thai'
+    # alphabet = 'vietnamese'
+    # alphabet = 'arabic'  # TODO: missing chars in fonts
+    # alphabet = 'hebrew'
+    # alphabet = 'khmer' # TODO: see comment in googlefonts
+    # alphabet = 'tamil'
+    # alphabet = 'gujarati' # TODO: one font to remove and one blank in another font
+    # alphabet = 'bengali'
+    # alphabet = 'malayalam'
+    # alphabet = 'korean' # TODO: huge amount of missing chars
+    # alphabet = 'chinese-simplified'
+    # alphabet = 'greek'
+    # alphabet = 'cyrillic'
 
-    print("Alphabets:")
-    for alphabet_name, alphabet in synbols.ALPHABET_MAP.items():
-        print("%s : %d fonts" % (alphabet_name, len(alphabet.fonts)))
 
-    # TODO: The TODOs next to each language are possibly out of date now that char centering works well and we check
-    #       fonts for missing chars. Need to check 100% of the languages and fonts for chars that don't render properly
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['latin'], 64, 64, 1, 20)  # TODO missing upper cases
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['telugu'], 64, 64, 1, 1)  # TODO the circle in some chars is not rendered. (bottom ones)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['thai'], 64, 64, 1, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['vietnamese'], 64, 64, 1, 1)
-    dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['arabic'], 64, 64, 1, 1)  # TODO: missing chars in fonts
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['hebrew'], 64, 64, 1, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['khmer'], 64, 64, 3, 1)  # TODO: see comment in googlefonts
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['tamil'], 64, 64, 1, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['gujarati'], 64, 64, 3, 1)  # TODO: one font to remove and one blank in another font
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['bengali'], 64, 64, 3, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['malayalam'], 64, 64, 3, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['korean'], 64, 64, 1000, 1)  # TODO: huge amount of missing chars
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['chinese-simplified'], 64, 64, 10, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['greek'], 64, 64, 1, 1)
-    # dataset = synbols.make_char_grid_from_lang(synbols.ALPHABET_MAP['cyrillic'], 64, 64, 1, 1)
+    fg = synbols.Camouflage(stroke_noise=0, stroke_angle=0.1, stroke_width=0.05, n_stroke=1000)
+    bg = synbols.Camouflage(stroke_noise=0, stroke_angle=1., stroke_width=0.05, n_stroke=1000)
 
-    plot_dataset(dataset)
+    ## Un comment to remove background
+    # fg, bg = None, None
+
+    kwargs = dict(foreground=fg, background=bg, is_bold=True, scale=(1.3, 1.3),
+                  resolution=(32, 32), rng=np.random.RandomState(42))
+
+    x, y = pack_dataset(generate_char_grid('latin', n_font=15, n_char=10, **kwargs))
+    plot_dataset(x, y, name=alphabet, h_axis='char', v_axis='font', rng=np.random.RandomState(42))
