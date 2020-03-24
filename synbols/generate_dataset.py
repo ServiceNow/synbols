42a4746bf5ca0002131bb60f18244542a78164e5
David Vazquez <dvazquez@elementai.com>
2020-03-24 16:03:53 -0400

    Solved bug with Segmentation generator

--- synbols/generate_dataset.py
+++ synbols/generate_dataset.py
@@ -95,27 +95,44 @@ def dataset_generator(attr_generator, n_samples, n_synbols_per_image=1):
             surface, ctxt = create_cairo_surface_and_ctxt(attributes[0].resolution)
             synbols._make_background(ctxt, attributes[0].background)
 
-            # Create the segmentation mask image with zeros
-            surfaceGT, ctxtGT = create_cairo_surface_and_ctxt(attributes[0].resolution)
-            synbols._make_background(ctxtGT, None)
-            synbols._make_foreground(ctxtGT, None)
+            # # Create the segmentation mask image with zeros
+            # surfaceGT, ctxtGT = create_cairo_surface_and_ctxt(attributes[0].resolution)
+            # synbols._make_background(ctxtGT, None)
+            # synbols._make_foreground(ctxtGT, None)
 
             y = []
+            x2 = np.zeros((attributes[0].resolution), dtype=np.uint8)
             # Draw each character
             for i, attrib in enumerate(attributes):
                 ctxt.save()
-                ctxtGT.save()
+                # ctxtGT.save()
                 attrib.draw_synbol(ctxt, select_background=False, select_foreground=True)
                 #ctxtGT.set_source_rgb(128*(i+1)/255.0, 128*(i+1)/255.0, 128*(i+1)/255.0)
-                ctxtGT.set_source_rgb((i+1)/255.0, (i+1)/255.0, (i+1)/255.0)
+                #ctxtGT.set_source_rgb((i+1.1)/256.0, (i+1.1)/256.0, (i+1.1)/256.0)
+                # Create the segmentation mask image with zeros
+                surfaceGT, ctxtGT = create_cairo_surface_and_ctxt(attributes[0].resolution)
+                synbols._make_background(ctxtGT, None)
+                synbols._make_foreground(ctxtGT, None)
+                # DRAW GT
                 attrib.draw_synbol(ctxtGT, select_background=False, select_foreground=False)
+                xGT = cairo_surface_to_GT(surfaceGT, attributes[0].resolution, attributes[0].rng, attributes[0].inverse_color, attributes[0].pixel_noise_scale)
+                xGT_mask = np.sum(xGT, axis=2) > 0
+                x2[xGT_mask] = (i+1)
                 ctxt.restore()
-                ctxtGT.restore()
+                # ctxtGT.restore()
                 y.append(attrib.attribute_dict())
             # Convert cairo surface into an image
             x = cairo_surface_to_image(surface, attributes[0].resolution, attributes[0].rng, attributes[0].inverse_color, attributes[0].pixel_noise_scale)
-            x2 = cairo_surface_to_GT(surfaceGT, attributes[0].resolution, attributes[0].rng, attributes[0].inverse_color, attributes[0].pixel_noise_scale)
-            # print(x2.min(), x2.max())
+            # x2 = cairo_surface_to_GT(surfaceGT, attributes[0].resolution, attributes[0].rng, attributes[0].inverse_color, attributes[0].pixel_noise_scale)
+
+            # x1_ = (x2 == 1) * 255
+            # x2_ = (x2 == 2) * 255
+            # from PIL import Image
+            # Image.fromarray(x1_.astype(np.uint8)).save('x1_gt.jpeg', quality=90)
+            # Image.fromarray(x2_.astype(np.uint8)).save('x2_gt.jpeg', quality=90)
+            # Image.fromarray(x2*128).save('xx2_gt.jpeg', quality=90)
+
+            # print(x2.min(), x2.max(), np.unique(x2))
 
         if i % 100 == 0 and i != 0:
             dt = (t.time() - t0) / 100.
