f2193e45a919ce149e618410d994e84cb2c7ad3a
Alexandre Drouin <alexandre.drouin1@gmail.com>
2020-03-30 18:48:57 -0400

    Import from synbols package

--- generator/view_dataset.py
+++ generator/view_dataset.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import defaultdict
-from data_io import load_npz
+from synbols.data_io import load_npz
 import logging
 
 logging.basicConfig(level=logging.INFO)
