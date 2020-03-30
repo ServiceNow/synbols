85b351f7d02c3b8d599c3e154d6f47d4d3292879
Alexandre Drouin <alexandre.drouin1@gmail.com>
2020-03-30 18:49:23 -0400

    Import from synbols package

--- generator/view_generator.py
+++ generator/view_generator.py
@@ -1,9 +1,9 @@
+import numpy as np
+
 from synbols.data_io import pack_dataset
-from synbols.drawing import Camouflage
+from synbols.drawing import Camouflage, color_sampler, Gradient, MultiGradient, NoPattern, SolidColor
 from synbols.generate import generate_char_grid
 from view_dataset import plot_dataset
-import synbols
-import numpy as np
 
 
 if __name__ == "__main__":
@@ -23,20 +23,20 @@ if __name__ == "__main__":
     # alphabet = 'greek'
     # alphabet = 'cyrillic'
 
-    # bg = synbols.Camouflage(stroke_angle=1.5)
-    # bg = synbols.NoPattern()
-    bg = synbols.MultiGradient(alpha=0.5, n_gradients=2, types=('linear', 'radial'))
-    # bg = synbols.Gradient(types=('linear',), random_color=synbols.color_sampler(brightness_range=(0.1, 0.9)))
+    # bg = Camouflage(stroke_angle=1.5)
+    # bg = NoPattern()
+    bg = MultiGradient(alpha=0.5, n_gradients=2, types=('linear', 'radial'))
+    # bg = Gradient(types=('linear',), random_color=color_sampler(brightness_range=(0.1, 0.9)))
 
-    # fg = synbols.Camouflage(stroke_angle=0.5)
-    # fg = synbols.SolidColor((1, 0, 0))
-    fg = synbols.Gradient(types=('radial',), random_color=synbols.color_sampler(brightness_range=(0.1, 0.9)))
-    # fg = synbols.NoPattern()
+    # fg = Camouflage(stroke_angle=0.5)
+    # fg = SolidColor((1, 0, 0))
+    fg = Gradient(types=('radial',), random_color=color_sampler(brightness_range=(0.1, 0.9)))
+    # fg = NoPattern()
 
     # kwargs = dict(foreground=fg, background=bg, is_bold=True, scale=(0.2, 0.2),
     #               resolution=(32, 32), rng=np.random.RandomState(42), n_symbols=20)
 
     kwargs = dict()
 
-    x, y = pack_dataset(generate_char_grid('latin', n_font=15, n_char=20, **kwargs))
+    x, y = pack_dataset(generate_char_grid('cyrillic', n_font=15, n_char=20, **kwargs))
     plot_dataset(x, y, name=alphabet, h_axis='char', v_axis='font', rng=np.random.RandomState(42))
