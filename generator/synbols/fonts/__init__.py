72c5c2a964aaa0d498f0d4353f7cedd00044ff3a
Alexandre Drouin <alexandre.drouin1@gmail.com>
2020-03-31 09:09:26 -0400

    Fix file not found error

--- generator/synbols/fonts/__init__.py
+++ generator/synbols/fonts/__init__.py
@@ -2,7 +2,6 @@ import os
 import pickle
 
 from .filters import filter_fonts
-from .google import GOOGLE_FONTS_METADATA_PATH
 from ..utils import Alphabet, SYMBOL_MAP
 
 
@@ -44,15 +43,7 @@ Zapfino
 """.splitlines()
 
 
-if not os.path.exists(GOOGLE_FONTS_METADATA_PATH):
-    # Fallback to a default alphabet map
-    # XXX: This should happen when running the code locally (not in the Docker image)
-    from warnings import warn
-    warn("Google fonts not found. Most likely, you are not running in the Docker image."
-         "Falling back to default fonts")
-    ALPHABET_MAP = {'latin': Alphabet('latin', DEFAULT_LATIN_FONT_LIST, SYMBOL_MAP['latin'])}
-
-else:
+try:
     # Build the list of alphabets and supported fonts (use cache if available)
     if os.path.exists(CACHE_FILE):
         ALPHABET_MAP = pickle.load(open(CACHE_FILE, "rb"))
@@ -64,3 +55,11 @@ else:
             alphabet.fonts = filter_fonts(alphabet)[0]
         pickle.dump(ALPHABET_MAP, open(CACHE_FILE, "wb"))
         del ALPHABET_MAP_
+        
+except FileNotFoundError:
+    # Fallback to a default alphabet map
+    # XXX: This should happen when running the code locally (not in the Docker image)
+    from warnings import warn
+    warn("Google fonts not found. Most likely, you are not running in the Docker image."
+         "Falling back to default fonts")
+    ALPHABET_MAP = {'latin': Alphabet('latin', DEFAULT_LATIN_FONT_LIST, SYMBOL_MAP['latin'])}
