06ab59de4f5d88e6d90b0611bbb95ea9fc9bc1a5
Pau Rodriguez <pau.rodri1@gmail.com>
2020-05-01 14:05:27 +0000

    Stratification

--- benchmarks/classification/models.py
+++ benchmarks/classification/models.py
@@ -8,7 +8,10 @@ from tqdm import tqdm
 import numpy as np
 from backbones import get_backbone
 import time
-
+try:
+    from apex import amp
+except:
+    pass
 
 def get_model(exp_dict):
     if exp_dict["model"] == 'classification':
@@ -31,6 +34,30 @@ class Classification(torch.nn.Module):
                                                                     factor=0.1,
                                                                     patience=10,
                                                                     verbose=True)
+        if self.exp_dict["amp"] > 0:
+            self.backbone, self.optimizer = amp.initialize(self.backbone, self.optimizer, opt_level="O%d" %self.exp_dict["amp"])
+
+    def backward(self, loss):
+        if self.exp_dict["amp"] > 0:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+    def vis_on_batch(self, x, y, name, gridsize=5):
+        import pylab
+        y = y.data.cpu().numpy()
+        b, c, h, w = x.size()
+        x = x.permute(0, 2, 3, 1).data.cpu().numpy()
+        x -= x.min()
+        x /= x.max()
+        y_set = np.unique(y)
+        rows = len(y_set)
+        cols = int(np.max([(y == _y).astype(float).sum() for _y in y_set]))
+        ret = np.zeros((rows, cols, h, w, 3))
+        for row, _y in enumerate(y_set):
+            _x = x[y == _y]
+            ret[row, :_x.shape[0], ...] = _x
+        pylab.imsave(name, ret.transpose((0, 2, 1, 3, 4)).reshape((h*rows, w*cols, 3)))
 
     def train_on_loader(self, loader):
         _loss = 0
@@ -51,8 +78,9 @@ class Classification(torch.nn.Module):
             _batch_time.append(time.time() - t)
             _loss += float(loss) * x.size(0)
             _total += x.size(0)
-            loss.backward()
+            self.backward(loss)
             self.optimizer.step()
+        # self.vis_on_batch(x, y, 'train.png')
         return {"train_loss": float(_loss) / _total,
                 "train_epoch_time": sum(_batch_time),
                 "train_batch_time": np.mean(_batch_time)}
@@ -82,6 +110,7 @@ class Classification(torch.nn.Module):
         _loss /= _total
         _accuracy /= _total
         self.scheduler.step(_loss)
+        # self.vis_on_batch(x, y, 'val.png')
         return {"val_loss": _loss,
                 "val_accuracy": _accuracy,
                 "val_epoch_time": sum(_batch_time),
