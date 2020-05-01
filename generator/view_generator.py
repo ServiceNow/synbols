baa44ed8b9b0c28913b4e580fb276c46ed1af8a9
recursix <alex.lacoste.shmu@gmail.com>
2020-05-01 15:51:49 -0400

    plot masks as well

--- generator/view_generator.py
+++ generator/view_generator.py
@@ -19,12 +19,17 @@ if __name__ == "__main__":
     fg = Gradient(types=('radial',), random_color=color_sampler(brightness_range=(0.1, 0.9)))
     # fg = NoPattern()
 
-    kwargs = dict(foreground=fg, background=bg, resolution=(32, 32), n_symbols=1,
-                  scale=lambda rng: 0.7 * np.exp(rng.randn() * 0.1),
-                  is_bold=True)
+    kwargs = dict(foreground=fg, background=bg, resolution=(64, 64), n_symbols=3,
+                  scale=lambda rng: 0.5 * np.exp(rng.randn() * 0.2),
+                  is_bold=False)
 
     # kwargs = dict()
 
-    x, y = pack_dataset(generate_char_grid('latin', n_font=15, n_char=20, **kwargs))
+    x, mask, y = pack_dataset(generate_char_grid('latin', n_font=15, n_char=20, **kwargs))
+    print(mask.shape, x.shape)
+    mask = mask.astype(np.float)/256
+
     plot_dataset(x, y, name='latin', h_axis='char', v_axis='font', rng=np.random.RandomState(42))
+    plot_dataset(mask, y, name='mask', h_axis='char', v_axis='font', rng=np.random.RandomState(42))
+
     plt.show()
