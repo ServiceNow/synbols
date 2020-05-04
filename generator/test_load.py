48424646abb2e4f185bf9d5eea9743f75caa9ee4
recursix <alex.lacoste.shmu@gmail.com>
2020-05-04 11:41:07 -0400

    script for testing save and load speed for h5py

--- generator/test_load.py
+++ generator/test_load.py
@@ -3,28 +3,35 @@ from synbols.data_io import load_h5, load_attributes_h5, load_minibatch_h5
 import time as t
 import h5py
 
-file_path = '../segmentation_n=100000_2020-Apr-30.h5py'
+file_path = 'test2.h5py'
 
 
-# print('load attributes')
-#
-# t0 = t.time()
-# attr_list, split = load_attributes_h5(file_path)
-# print("took %.2fs" % (t0 - t.time()))
-#
-# print('load dataset')
-#
-# t0 = t.time()
-# x, mask, attr_list, splits = load_h5(file_path)
-# print("took %.3fs" % (t0 - t.time()))
+# file_path = '../segmentation_n=10000_2020-May-04.h5py'
+
+
+def write_ds(file_path, n_samples, chunk_size=1):
+    with h5py.File(file_path, 'w', libver='latest') as file:
+        shape = (32, 32, 3)
+        dset = file.create_dataset("x", (n_samples,) + shape, dtype=np.uint8)
+
+        for i in range(n_samples):
+            if i % 1000 == 0:
+                print('writing %d' % i)
+            dset[i] = np.random.randint(0, 2, shape)
+
 
 def random_indices():
+    return np.sort(np.random.choice(5000, 100, replace=False))
 
-    # return np.sort(np.random.choice(100000, 100, replace=False))
-    #
-    start = np.random.randint(0, 10000)
+    start = np.random.randint(0, 5000)
     return np.arange(start, start + 100)
 
+
+n_samples = 10000
+t0 = t.time()
+write_ds(file_path, n_samples)
+dt = (t.time() - t0) / n_samples
+print('writing: took %.3g ms/images' % (dt * 1000))
 
 for i in range(10):
     indices = random_indices()
@@ -33,17 +40,5 @@ for i in range(10):
     with h5py.File(file_path, 'r') as fd:
         x = np.array(fd['x'][indices])
 
-    print('took %.3gs for minibatch' % (t.time() - t0))
-
-print(x.shape)
-
-print()
-print("open one time")
-with h5py.File(file_path, 'r') as fd:
-    for i in range(10):
-
-        t0 = t.time()
-        indices = random_indices()
-        x = np.array(fd['x'][indices])
-
-        print('took %.3gs for minibatch' % (t.time() - t0))
+    dt = (t.time() - t0) / len(indices)
+    print('reading: took %.3g ms/images, x.shape=%s' % ((dt * 1000), x.shape))
