d6d7ca82246651f474f73bfcedd278665a90c841
recursix <alex.lacoste.shmu@gmail.com>
2020-05-19 15:35:23 -0400

    add script to run in docker

---
+++ docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "$@"
+
+synbol_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+docker run --user $(id -u) -it \
+    -v $synbol_dir:/local \
+    -w /local \
+    synbols \
+    python "$@"
