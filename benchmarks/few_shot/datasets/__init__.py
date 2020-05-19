a0c66373cde0a5d7110ded1633e5cde90820236f
Pau Rodriguez <pau.rodri1@gmail.com>
2020-05-19 21:02:21 +0000

    Add miniimagenet

--- benchmarks/few_shot/datasets/__init__.py
+++ benchmarks/few_shot/datasets/__init__.py
@@ -2,6 +2,7 @@ from torchvision import transforms as tt
 from .synbols import SynbolsFolder, SynbolsNpz
 from .episodic_dataset import FewShotSampler # should be imported here because it changes the dataloader to be episodic
 from .episodic_synbols import EpisodicSynbols 
+from .episodic_miniimagenet import EpisodicMiniImagenet
 import sys
 
 def get_dataset(split, exp_dict):
@@ -29,5 +30,16 @@ def get_dataset(split, exp_dict):
                                 key=dataset_dict["task"], 
                                 transform=transform,
                                 mask=exp_dict['dataset']['mask'])
+    elif dataset_dict["name"] == "miniimagenet":
+        transform = tt.Compose([tt.ToPILImage(), tt.ToTensor()])
+        sampler = FewShotSampler(nclasses=dataset_dict["nclasses_%s" %split],
+                                 support_size=dataset_dict["support_size_%s" %split],
+                                 query_size=dataset_dict["query_size_%s" %split],
+                                 unlabeled_size=0)
+        return EpisodicMiniImagenet(dataset_dict["path"],
+                                split=split, 
+                                sampler=sampler, 
+                                size=dataset_dict["%s_iters" %split], 
+                                transform=transform)
     else:
         raise ValueError("Dataset %s not found" % dataset_dict["name"])
