325052c3030f9ad72e5ffe110da582a83aad35c4
recursix <alex.lacoste.shmu@gmail.com>
2020-05-20 10:12:15 -0400

    remove interactive shell from docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 synbol_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docker run --user $(id -u) -it \
+docker run --user $(id -u) \
     -v $synbol_dir:/local \
     -w /local \
     synbols \
