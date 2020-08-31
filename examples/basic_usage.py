60fff1198c7df1bac9372c1c4bebc03caceaac1e
recursix <alex.lacoste.shmu@gmail.com>
2020-08-31 08:30:23 -0400

    Make the first example slightly more complex.

--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,5 +1,3 @@
-from synbols.generate import generate_and_write_dataset, basic_image_sampler
-
 """
 Example Usage:
 ````
@@ -14,11 +12,18 @@ Alternatively, you can mount any extra directory with the arguement `--mount-pat
 location by changing the `dataset_path` variable to point to that location.
 """
 
-dataset_path = "./fix_scale_dataset"
+from synbols.generate import generate_and_write_dataset, basic_image_sampler
+
+dataset_path = "./large_translation_dataset"
 n_samples = 1000
 
-# Modifies the default attribute sampler to fix the scale to 0.8
-attr_sampler = basic_image_sampler(scale=0.8)
+
+def translation(rng):
+    return tuple(rng.uniform(low=-2, high=2, size=2))
+
+
+# Modifies the default attribute sampler to fix the scale to a constant and the (x,y) translation to a new distribution
+attr_sampler = basic_image_sampler(scale=0.5, translation=translation)
 
 generate_and_write_dataset(dataset_path, attr_sampler, n_samples)
 
