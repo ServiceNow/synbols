71107eac72e46a1757e06b6f3c51ea7836cf0d9d
recursix <alex.lacoste.shmu@gmail.com>
2020-09-02 14:09:40 -0400

    make the generate_dataset more accessible from synbols run

--- scripts/generate_dataset.py
+++ scripts/generate_dataset.py
@@ -1,30 +1,27 @@
 #!/usr/bin/env python
 import argparse
 import logging
+from datetime import datetime
+from synbols.generate import DATASET_GENERATOR_MAP, make_preview
+from synbols.data_io import write_h5
+from synbols.fonts import ALPHABET_MAP
 
 logging.basicConfig(level=logging.INFO)
 
-from datetime import datetime
-from synbols.fonts import ALPHABET_MAP
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', help='name of the predefined dataset', default='default')
+
+dataset_names = ' | '.join(DATASET_GENERATOR_MAP.keys())
+alphabet_names = ' | '.join(ALPHABET_MAP.keys())
+
+parser.add_argument('--dataset', help='Name of the predefined dataset. One of %s' % dataset_names, default='default')
 parser.add_argument('--n_samples', help='number of samples to generate', type=int, default=10000)
-parser.add_argument('--alphabet', help='of the alphabet to use', default='default')
+parser.add_argument('--alphabet', help='Which alphabet to use. One of %s' % alphabet_names, default='default')
 parser.add_argument('--resolution', help="""Image resolution e.g.: "32x32". Defaults to the dataset's default.""",
                     default='default')
 
 if __name__ == "__main__":
 
     args = parser.parse_args()
-
-    from synbols.generate import DATASET_GENERATOR_MAP, make_preview
-    from synbols.data_io import write_h5
-
-    for alphabet_name, alphabet in ALPHABET_MAP.items():
-        logging.info("%s: %d symbols, %d fonts", alphabet_name, len(alphabet.symbols), len(alphabet.fonts))
-
-    logging.info("Generating %d samples from %s dataset", args.n_samples, args.dataset)
 
     if args.alphabet == 'default':
         alphabet = 'latin'
