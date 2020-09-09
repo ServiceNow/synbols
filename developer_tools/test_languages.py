446086f74b2b509f8a63c9acf52f81f0fb2e5946
Alexandre Drouin <alexandre.drouin1@gmail.com>
2020-09-09 15:33:56 -0400

    Updated path

--- developer_tools/test_languages.py
+++ developer_tools/test_languages.py
@@ -1,5 +1,5 @@
 from synbols.utils import Language, load_all_languages
 
-print(Language(locale_file="../locale_el_greek.npz").get_alphabet(standard=True, auxiliary=True, lower=True, upper=True, support_bold=True))
+print(Language(locale_file="/locales/locale_el_greek.npz").get_alphabet(standard=True, auxiliary=True, lower=True, upper=True, support_bold=True))
 
 print(load_all_languages())
