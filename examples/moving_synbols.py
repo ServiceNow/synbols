1dbd30998c8fbd6127d4508984ca57f4c276143c
recursix <alex.lacoste.shmu@gmail.com>
2021-01-04 16:31:22 -0500

    adjust values for generating first version

--- examples/moving_synbols.py
+++ examples/moving_synbols.py
@@ -7,13 +7,12 @@ def transition_function(scene, masks):
     update_scene(scene, masks)
     for symbol in scene.symbols:
         symbol.symbol.translation += 0.05 * np.random.randn(2)
-        symbol.speed += 0.2 * np.random.randn(2)
-        symbol.angular_speed += 0.2 * np.random.randn(1)
-        symbol.symbol.scale += 0.05 * np.random.randn(1)
+        symbol.speed += 0.01 * np.random.randn(2)
+        symbol.angular_speed += 0.01 * np.random.randn(1)
+        symbol.symbol.scale += 0.01 * np.random.randn(1)
 
 
+attr_sampler = basic_attribute_sampler(resolution=(32, 32), n_symbols=1, scale=0.5, max_contrast=False)
+scene_sampler = dynamic_scene_sampler(attr_sampler, transition_function=transition_function, time_steps=500)
 
-attr_sampler = basic_attribute_sampler(resolution=(32, 32), n_symbols=3)
-scene_sampler = dynamic_scene_sampler(attr_sampler, transition_function=transition_function, time_steps=100)
-
-generate_and_write_dataset('synbols_motion_2', scene_sampler, 25, preview_shape=(5, 5))
+generate_and_write_dataset('synbols_motion_2', scene_sampler, 1000, preview_shape=(5, 5))
